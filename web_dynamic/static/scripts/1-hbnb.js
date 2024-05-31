49c8278cb909997e07dadba7351f6416550cec7c
root <root@DESKTOP-LP5R8B0>
2024-05-31 16:24:09 +0300

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     $('INPUT.checkbox').change(function () {
-        if ($('.checkbox').checked)
-            {
-            }
+        if ($(this).is(":checked")) {
+            amenities[$(this).attr('data-id')] = $(this).attr('data-name')
+        }
+        else {
+            delete amenities[$(this).attr('data-id')]
+        }
+        $(".amenities H4").text(Object.values(amenities).join(', '));
     })
-  });
+});
