e9102333a93faa85cf06ec7be4b20884a5b1593e
root <root@DESKTOP-LP5R8B0>
2024-05-31 16:42:34 +0300

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    const amenities = {};
     $('INPUT[type="checkbox"]').change(function () {
         if ($(this).is(":checked")) {
             amenities[$(this).attr('data-id')] = $(this).attr('data-name')
