c8144e22e6195748a14018ff43b30ad18028e40a
root <root@DESKTOP-LP5R8B0>
2024-06-01 18:48:44 +0300

    task 5

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    const amenities = {};
+    const amenityIds = {};
     $('INPUT[type="checkbox"]').change(function () {
         if ($(this).is(":checked")) {
-            amenities[$(this).attr('data-id')] = $(this).attr('data-name')
+            amenityIds[$(this).attr('data-id')] = $(this).attr('data-name')
         }
         else {
-            delete amenities[$(this).attr('data-id')]
+            delete amenityIds[$(this).attr('data-id')]
         }
-        $(".amenities H4").text(Object.values(amenities).join(', '));
+        $(".amenities H4").text(Object.values(amenityIds).join(', '));
     })
 
     $.get('http://localhost:5001/api/v1/status/', function (data) {
@@ -17,7 +17,40 @@ $(document).ready(function () {
             $('#api_status').removeClass('available');
         }
     });
-    $('.filters button').click(function () {
+    function fetchPlaces() {
+        const PLACES_URL = `http://localhost:5001/api/v1/places_search/`;
+        $.ajax({
+            url: PLACES_URL,
+            type: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            data: JSON.stringify({ amenities: Object.values(amenityIds) }),
+            success: function (response) {
+                $('SECTION.places').empty();
+                for (const r of response) {
+                    const article = ['<article>',
+                        '<div class="title_box">',
+                        `<h2>${r.name}</h2>`,
+                        `<div class="price_by_night">$${r.price_by_night}</div>`,
+                        '</div>',
+                        '<div class="information">',
+                        `<div class="max_guest">${r.max_guest} Guest(s)</div>`,
+                        `<div class="number_rooms">${r.number_rooms} Bedroom(s)</div>`,
+                        `<div class="number_bathrooms">${r.number_bathrooms} Bathroom(s)</div>`,
+                        '</div>',
+                        '<div class="description">',
+                        `${r.description}`,
+                        '</div>',
+                        '</article>'];
+                    $('SECTION.places').append(article.join(''));
+                }
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+    fetchPlaces({});
+    $('.searchBtn').click(function () {
         const PLACES_URL = `http://localhost:5001/api/v1/places_search/`;
         $.ajax({
             url: PLACES_URL,
@@ -49,6 +82,5 @@ $(document).ready(function () {
             }
         });
     })
-
 
 });
