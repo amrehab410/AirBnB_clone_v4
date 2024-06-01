6de22d4b68c2b4b2f8c0850683d50f785e335f7c
root <root@DESKTOP-LP5R8B0>
2024-06-01 15:46:57 +0300

    task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -10,4 +10,43 @@ $(document).ready(function () {
         $(".amenities H4").text(Object.values(amenities).join(', '));
     })
 
+    $.get('http://localhost:5001/api/v1/status/', function (data) {
+        if (data.status == 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
 });
+
+function fetchPlaces() {
+    const PLACES_URL = `http://localhost:5001/api/v1/places_search/`;
+    $.ajax({
+        url: PLACES_URL,
+        type: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        data: JSON.stringify({}),
+        success: function (response) {
+            for (const r of response) {
+                const article = ['<article>',
+                    '<div class="title_box">',
+                    `<h2>${r.name}</h2>`,
+                    `<div class="price_by_night">$${r.price_by_night}</div>`,
+                    '</div>',
+                    '<div class="information">',
+                    `<div class="max_guest">${r.max_guest} Guest(s)</div>`,
+                    `<div class="number_rooms">${r.number_rooms} Bedroom(s)</div>`,
+                    `<div class="number_bathrooms">${r.number_bathrooms} Bathroom(s)</div>`,
+                    '</div>',
+                    '<div class="description">',
+                    `${r.description}`,
+                    '</div>',
+                    '</article>'];
+                $('SECTION.places').append(article.join(''));
+            }
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
